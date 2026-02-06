9eeb82265e3542eec530516817d59da2f3ef354c
apanoinvestments <vadym.voytas@apano.de>
2026-02-06 13:50:53 +0100

    Add Decap CMS admin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+// export default defineConfig({});
+export default defineConfig({
+  site: "https://example.netlify.app", // später echte Domain
+});
